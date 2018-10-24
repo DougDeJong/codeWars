589ae6cccb5cd62951e64ce2f610e9a30431b8fd
Doug DeJong <dougdejong@gmail.com>
2018-10-24 17:53:23 -0400

    some fixes

--- JavaScript/resistorColors.js
+++ JavaScript/resistorColors.js
@@ -1,39 +1,35 @@
 function encodeResistorColors(ohmsString) {
-  
+
   let resistColors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'gray', 'white'];
-  
   let split = ohmsString.split(" ");
-    
   let resistVal = split[0];
-    
+  
     if (resistVal.includes('k')) {
-    
+      
       resistVal = resistVal.replace('k', '000')
+      
+      }
     
-  }
-  
-    if (resistVal.includes('M'))  {
-      resistVal = resistVal.replace('M', '000000')
-  }
-    
-    if (resistVal.includes('.'))  {
-      resistVal = resistVal.replace('.', '');
-      resistValNum = Number(resistVal);
-      resistVal = (resistValNum/10).toString();
-    }
-  
-    
+    if (resistVal.includes('M')) {
+        resistVal = resistVal.replace('M', '000000')
+      }
+      
+    if (resistVal.includes('.')) {
+    resistVal = resistVal.replace('.', '');
+    resistValNum = Number(resistVal);
+    resistVal = (resistValNum/10).toString();
+      }
+      
     let bandString = '';
     
-    for (let i = 0; i < 2; i++) {
-      bandString += (resistColors[(Number(resistVal[i]))] + ' ')
+    for (let i=0; i < 2; i++) {
+      bandString += (resistColors[Number(resistVal[i])] + ' ')
+      }
+      
+    if (resistVal.length > 3) {
+    return (( bandString += resistColors[resistVal.length - 2]) + ' ' + 'gold')
     }
     
-    if (resistVal.length > 3) { 
-    return (( bandString += resistColors[resistVal.length -2]) + ' ' + 'gold')};
-  
-    
-    return ((bandString += resistColors[resistVal.length -2]) + ' ' + 'gold')
-  
-  
+    return (( bandString  += resistColors[resistVal.length - 2]) + ' ' + 'gold')
+      
   }
